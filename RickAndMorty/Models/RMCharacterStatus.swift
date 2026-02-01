024c878f11edf4b48b81f828fe869ff52bc9b354
harungunes <h.gunes13@gmail.com>
2026-02-01 17:34:31 +0100

    feat: configure models

---
+++ RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMorty
+//
+//  Created by Harun Gunes on 01/02/2026.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,8 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Harun Gunes on 01/02/2026.
+//
+
+import Foundation
